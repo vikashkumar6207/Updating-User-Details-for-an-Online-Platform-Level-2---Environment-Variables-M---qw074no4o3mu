4cce094a3a543b4e6a792a2ae36c2df0f4780b04
vikashkumar6207 <vktd828403@gmail.com>
2024-06-01 05:45:49 +0530

    Don

--- src/app.js
+++ src/app.js
@@ -11,6 +11,34 @@ const userDetails = JSON.parse(
 app.use(express.json());
 
 // Write PATCH endpoint for editing user details
+app.patch("/api/v1/details/:id", (req, res) => {
+  const id = req.params.id * 1;
+  const updateDetails = userDetails.find(
+    (updateDetails) => updateDetails.id === id
+  );
+  const index = userDetails.indexOf(updateDetails)
+  if(!updateDetails){
+    return res.status(404).send({
+      status : "failed",
+      message: "User not found!"
+    })
+  }
+  Object.assign(updateDetails, req.body);
+
+fs.writeFile(`${__dirname}/data/userDetails.json`, 
+JSON.stringify(userDetails),
+(err) => {
+  res.status(200).json({
+    status: "Success",
+    message: `User details updated successfully for id: ${updateDetails.id}`,
+    data: {
+      userDetails: updateDetails,
+    },
+  });
+}
+)
+});
+
 
 // POST endpoint for registering new user
 app.post("/api/v1/details", (req, res) => {
